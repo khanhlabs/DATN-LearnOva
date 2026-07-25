1739cce90b8238ba1c9fdde28fbbebefa6c9413c
rikai-tanhieu <hieu.doan@rikai.technology>
2026-07-25 10:16:13 +0700

    thay approuter như main

--- front_end/src/route/AppRoutes.jsx
+++ front_end/src/route/AppRoutes.jsx
@@ -23,10 +23,13 @@ import OverviewPage from "../page/teacher/overview/OverviewPage.jsx";
 import CoursesPage from "../page/teacher/courses/CoursesPage.jsx";
 import CourseCreationPage from "../page/teacher/courses/create/CourseCreationPage.jsx";
 import PromotionsPage from "../page/teacher/promotions/PromotionsPage.jsx";
+import AnnouncementsPage from "../page/teacher/announcements/AnnouncementsPage.jsx";
 import StudentsPage from "../page/teacher/students/StudentsPage.jsx";
 import ReviewsPage from "../page/teacher/reviews/ReviewsPage.jsx";
+import QAInboxPage from "../page/teacher/qa/QAInboxPage.jsx";
 import RevenuePage from "../page/teacher/revenue/RevenuePage.jsx";
 import AnalyticsPage from "../page/teacher/analytics/AnalyticsPage.jsx";
+import ProfilePage from "../page/teacher/profile/ProfilePage.jsx";
 import UserLayout from "../layout/user/UserLayout.jsx";
 import InstructorsPage from "../page/users/intructor.jsx";
 import InstructorDetail from "../page/users/intructor/intructorDetail/intructorDetail.jsx";
@@ -39,6 +42,10 @@ import Cart from "../page/home/cart/Cart.jsx";
 import OAuth2Success from "../page/auth/OAuth2Success.jsx";
 import PaymentSuccess from "../page/home/payment/PaymentSuccess.jsx";
 import PaymentCancel from "../page/home/payment/PaymentCancel.jsx";
+import ApplyTeacherPage from "../page/teacherApplication/ApplyTeacherPage.jsx";
+import TeacherApplicationPage from "../page/admin/teacherApplication/TeacherApplicationPage.jsx";
+import AdminPayoutRequestsPage from "../page/admin/payoutRequests/AdminPayoutRequestsPage.jsx";
+import VerifyCertificatePage from "../page/certificate/VerifyCertificatePage.jsx";
 
 
 const App = () => {
@@ -48,13 +55,14 @@ const App = () => {
 
                 <Route path="/learnova/auth/login" element={<AuthPage/>}/>
                 <Route path="/learnova/user/courses-detail" element={<CourseDetail/>}/>
-                <Route path="/learnova/user/CoursesDetail/:courseId" element={<CourseDetail/>}/>
+                <Route path="/learnova/user/courses-detail/:courseId" element={<CourseDetail/>}/>
                 <Route path="/oauth2-success" element={<OAuth2Success />}/>
                 <Route path="/payment/success" element={<PaymentSuccess />}/>
                 <Route path="/payment/cancel" element={<PaymentCancel />}/>
+                <Route path="/learnova/certificate/verify/:code" element={<VerifyCertificatePage/>}/>
 
                 <Route element={<HomeLayout/>}>
-                    <Route path="/learnova/home" element={<Home/>}/>
+                    <Route path="/" element={<Home/>}/>
                 </Route>
 
                 {/* Admin */}
@@ -75,6 +83,10 @@ const App = () => {
                     <Route path="categories" element={<Category/>}/>
                     <Route path="tags" element={<Tag/>}/>
                     <Route path="violation-reports" element={<ViolationReports/>}/>
+                    <Route path="teacher-applications" element={<TeacherApplicationPage/>}/>
+                    <Route path="teacher-applications/:applicationId" element={<TeacherApplicationPage/>}/>
+                    <Route path="payout-requests" element={<AdminPayoutRequestsPage/>}/>
+                    <Route path="payout-requests/:requestId" element={<AdminPayoutRequestsPage/>}/>
                 </Route>
 
                 {/* Teacher */}
@@ -84,10 +96,13 @@ const App = () => {
                     <Route path="courses/create" element={<CourseCreationPage/>}/>
                     <Route path="courses/edit/:courseId" element={<CourseCreationPage/>}/>
                     <Route path="promotions" element={<PromotionsPage/>}/>
+                    <Route path="announcements" element={<AnnouncementsPage/>}/>
                     <Route path="students" element={<StudentsPage/>}/>
                     <Route path="reviews" element={<ReviewsPage/>}/>
+                    <Route path="qna" element={<QAInboxPage/>}/>
                     <Route path="revenue" element={<RevenuePage/>}/>
                     <Route path="analytics" element={<AnalyticsPage/>}/>
+                    <Route path="profile" element={<ProfilePage/>}/>
                 </Route>
 
                 {/* User */}
@@ -96,7 +111,7 @@ const App = () => {
                     <Route path="/learnova/cart" element={<Cart/>}/>
                     <Route path="/learnova/courses/detail/:id" element={<CourseDetaill/>}/>
                     <Route path="/learnova/intructors" element={<InstructorsPage/>}/>
-                    <Route path="/learnova/intructorDetail/:id" element={<InstructorDetail/>}/>
+                    <Route path="/learnova/intructorDetail/:instructorId" element={<InstructorDetail/>}/>
                     <Route path="/learnova/about" element={<AboutView/>}/>
 
                     <Route element={<RequireRole><Outlet/></RequireRole>}>
@@ -104,6 +119,7 @@ const App = () => {
                         <Route path="/learnova/user/profile/courses" element={<ProfileViewProps key="courses" initialTab="courses"/>}/>
                         <Route path="/learnova/user/profile/favorites" element={<ProfileViewProps key="favorites" initialTab="favorites"/>}/>
                         <Route path="/learnova/user/profile/security" element={<ProfileViewProps key="security" initialTab="security"/>}/>
+                        <Route path="/learnova/apply-teacher" element={<ApplyTeacherPage/>}/>
                     </Route>
                 </Route>
 
